5df6557e3f9311c7970cc6e43bca3918047e3932
dnlsouz <daniel.checca@gmail.com>
2023-10-18 15:52:45 -0300

    18-10

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -1,123 +1,101 @@
-"use strict"
+"use strict";
 
+/* Sobre funções
 
+Funções são blocos de código que
+podem ser executados e reaproveitados
+em praticamente qualquer lugar de
+uma aplicação/site.
 
-/*  Sobre funções
+Usar funções também é benéfico do
+ponto de vista de organização de código.
 
- 
-
-Funções são blocos de códigos que podem ser executados e reaproveitados em praticamente qualquer lugar de uma aplicação/site.
-
- 
-
-Usar funções também é benéfico do ponto de vista de organização de código.
-
- 
-
-O JS já possui centenas de funções prontas.
-
-Exemplos: log(), alert(), prompt(), toFixed() etc*/
-
-
-
-
-
-
+O JS já possui centenas de funções prontas. 
+Exemplos: log(), alert(), prompt(), toFixed() etc */
 
 /* Como escrever funções no JS? */
 
-
-
 // Forma 1: função anônima
-
-
-
-const exemplo1 = function () {
-
+const exemplo1 = function(){
     console.log("Função anônima");
-
-}
-
-
-
-
+};
 
 // Forma 2: função nomeada/declarada
-
-
-
-function exemplo2() {
-
+function exemplo2(){
     console.log("Função nomeada/declarada");
-
 }
-
-
-
-
 
 // Forma 3: arrow function
-
-
-
 const exemplo3 = () => {
-
-    console.log("Arrow function");
-
-}
-
-
-
-
+    console.log("Arrow Function");
+};
 
 // Chamada de função para execução
+exemplo1();
+exemplo2();
+exemplo3();
 
-exemplo1()
+/* Exemplos diversos */
 
-exemplo2()
-
-exemplo3()
-
- 
-
-// Exemplos diversos//
-
-/* Função com parâmetros e retornos de dados /resultados */
-
-function somar(valor1,valor2){
-   return valor1 + valor2;
+/* Função com parâmetros e retorno
+de dados/resultados */
+function somar(valor1, valor2){
+    return valor1 + valor2;
 }
 
-/* chamamos a função diversas vezes e passando valores diferentes como parâmetros */
+/* Chamamos a função diversas vezes
+e passando valores diferentes como
+parâmetros. */
+console.log( somar(10, 20) );
+console.log( somar(15, 50) );
+console.log( somar(30, 47.8) );
 
-console.log(somar(10,20));
-console.log(somar(15,50));
-console.log(somar(30,47.8));
 
-/* Formatação de valores em reais */
-let preco =5500;
-let desconto = preco * 0.10; //10%
+/* Formatação de valores em Reais */
+let preco = 5500;
+let desconto = preco * 0.10; // 10%
 let precoFinal = preco - desconto;
 
-function formatarPreco (valor){
-    let opcoes = { 
-        style: "currency", 
-        currency: "BRL"   
-       }
+function formatarPreco(valor){
+    let opcoes = {
+        style: "currency",
+        currency: "BRL"
+    };
 
-       return valor.toLocaleString("pt-br", opcoes);
+    return valor.toLocaleString("pt-br", opcoes);
 }
 
 console.log(`Preço: ${formatarPreco(preco)}`);
+console.log(`Desconto: ${formatarPreco(desconto)}`);
+console.log(`Preço final: ${formatarPreco(precoFinal)}`);
 
-console.log(`Desconto ${formatarPreco(desconto)}`);
+console.log("------------------");
 
-console.log(`Preço Final: ${formatarPreco(precoFinal)}`);
+/* Sobre Arrow Function
+Sintaxe moderna para funções no JS,
+bastante usada por bibliotecas, frameworks 
+(React, Angular, Vue e etc). 
 
+A sintaxe geral é:
 
+const algumNome = () => {};
 
+No entanto, esta sintaxe pode ser mais concisa. */
 
+// Versão 1
+// function dobra(valor){
+//     return valor * 2;
+// }
 
+// Versão 2 (Arrow Function)
+// const dobra = (valor) => {
+//     return valor * 2;
+// };
 
+// Versão 3 (Arrow Function com retorno ímplicito)
+const dobra = valor => valor * 2;
 
-
+// Chamadas
+console.log( dobra(10) );
+console.log( dobra(1325) );
+console.log( dobra(55.47) );
--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,13 +1,30 @@
-"use strict"
+"use strict";
 
+// Dados de entrada (exemplos de valores)
+let valorDaCotacaoDoDia = 5.03;
+let valorEmDolares = 2500;
 
-let dolar = 1000
-let cotacao
-
-
-function converteMoeda (cotacao){
-    return dolar  * 5,03;
-    let convesao = 
+// Função que realiza a conversão matemática ao receber PARÂMETROS
+function converteMoeda( valor, cotacao ){
+    return valor * cotacao;
 }
 
-console.log
+// Variável que receberá o resultado processado pela função DEPOIS que
+// a função terminar sua execução
+let valorConvertido = converteMoeda(valorDaCotacaoDoDia, valorEmDolares);
+
+// Saídas após o processamento
+//console.log(valorEmDolares); // quanto que era em dólares
+//console.log(valorConvertido); // quanto que fica em reais
+
+console.log( formatarMoeda(valorEmDolares, "en", "USD") );
+console.log( formatarMoeda(valorConvertido, "pt-br", "BRL") );
+
+function formatarMoeda(valor, local, moeda){
+    let opcoes = {
+        style: "currency",
+        currency: moeda
+    };
+
+    return valor.toLocaleString(local, opcoes);
+}
